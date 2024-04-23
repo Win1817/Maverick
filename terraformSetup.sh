bf1b465bfce4854a913bba1f40f6457fa4bbcf94
Surely Win Dilag <108412632+Win1817@users.noreply.github.com>
2024-04-23 05:08:55 +0200

    Create terraformSetup.sh

---
+++ terraformSetup.sh
@@ -0,0 +1,7 @@
+wget https://releases.hashicorp.com/terraform/1.8.1/terraform_1.8.1_linux_amd64.zip
+
+unzip terraform_1.8.1_linux_amd64.zip
+
+sudo mv terraform /usr/local/bin/
+
+terraform --version
